495ff6691d04df1bf7079f2adc33757ac9ff1207
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-05-20 18:15:54 +0530

    linkedin update

--- linkedinloginapi/index.php
+++ linkedinloginapi/index.php
@@ -9,14 +9,19 @@
 </head>
 
 <body>
+
     <?php
-require_once 'config.php';
+require_once 'linkedin_config.php';
  
 try {
     $adapter->authenticate();
     $userProfile = $adapter->getUserProfile();
     print_r($userProfile);
     echo '<a href="logout.php">Logout</a>';
+
+    echo '<br><img src="'.$userProfile ->photoURL.'" style="width:100px;" alt="">';
+    echo '<br>Name :'.$userProfile ->firstName.' '.$userProfile ->lastName;
+    echo '<br>Email :'.$userProfile ->email;
 }
 catch( Exception $e ){
     echo $e->getMessage() ;
